f476d5cbf1177affed7ed8dde451bc7db1a007ab
~Alpesh | Your's Jarvis <77638593+Yours-Jarvis@users.noreply.github.com>
2021-12-23 12:49:12 +0530

    Area of Rectangle 🙃

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/**| Program of Area of Rectangle with is made with C++ Language 🙃|**/
+
+/**| 🚀 Please follow me on GitHub to stay tuned with us for more Exciting future Updates like this. | © 2021 — Made By ~Alpesh | Your's Jarvis #2431 with ♥ |**/
+
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main() {
+   // Declare Variables
+   int length, breadth, area;
+
+  cout << "\n";
+
+   cout << "• Simple C++ Program :|=> Area Of Rectangle\n";
+   cout << "\n• Enter the Length of Rectangle :|=> ";
+   cin>>length;
+
+   cout << "\n• Enter the Breadth of Rectangle :|=> ";
+   cin>>breadth;
+
+   area = length * breadth;
+   cout << "\n•|=> Area of Rectangle is :|=> " << area;
+
+   
+   return (0);
+  }
+
+/**| © 2021 — Coded By ~Alpesh | Your's Jarvis... |**/
+
+/**| © Do not share code without is credit's 🙃 |**/
